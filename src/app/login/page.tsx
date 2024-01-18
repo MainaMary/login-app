5d750bd013cadb51ee879722921db99a3c56ce19
Mary <marieshix98@gmail.com>
2024-01-18 23:55:10 +0300

    define context for handling auth

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginForm from "../_components/login";
+
+const Login = () => {
+  return <LoginForm />;
+};
+
+export default Login;
--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,14 +1,52 @@
-import { useContext, createContext, ReactNode } from "react";
+"use client";
+import {
+  useContext,
+  createContext,
+  ReactNode,
+  Dispatch,
+  SetStateAction,
+} from "react";
 import { useState } from "react";
 interface Props {
   children: ReactNode;
 }
-const AuthContext = createContext({});
+interface AuthContextProps {
+  formValues: {
+    name: string;
+    password: string;
+    email: string;
+    confirmPassword: string;
+  };
+  setFormValues: Dispatch<
+    SetStateAction<{
+      name: string;
+      password: string;
+      email: string;
+      confirmPassword: string;
+    }>
+  >;
+}
+const defaultValue: AuthContextProps = {
+  formValues: {
+    name: "",
+    password: "",
+    email: "",
+    confirmPassword: "",
+  },
+  setFormValues: () => {},
+};
+const AuthContext = createContext<AuthContextProps>(defaultValue);
 export const useAuth = () => useContext(AuthContext);
+
 export const AuthContextProvider = ({ children }: Props) => {
-  const [formValues, setFormValues] = useState({});
+  const [formValues, setFormValues] = useState({
+    name: "",
+    password: "",
+    email: "",
+    confirmPassword: "",
+  });
   return (
-    <AuthContext.Provider value={{ formValues }}>
+    <AuthContext.Provider value={{ formValues, setFormValues }}>
       {children}
     </AuthContext.Provider>
   );
